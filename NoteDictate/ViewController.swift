a672932e0fa20d6c6ad2ce0045073966464861a4
Praveen <praveenislive@gmail.com>
2020-03-08 22:17:49 +0530

    Initial Commit

---
+++ NoteDictate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteDictate
+//
+//  Created by Praveen on 08/03/20.
+//  Copyright © 2020 Praveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
